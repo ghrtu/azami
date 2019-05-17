e6afe5cc993aaa9a8e2852fb037b3a6a40102aea
rinako <g17912hr@gm.tsuda.ac.jp>
2019-05-17 17:20:45 +0900

    数値を英語に変えるプログラム

---
+++ src/azamii/IntToEng.java
@@ -0,0 +1,53 @@
+package azamii;
+
+import java.util.Scanner;
+
+public class IntToEng {
+	
+	// メインメソッド
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        System.out.println(translateEng(input));
+
+    }
+
+    // 数値を英訳する変換するメソッド
+
+    static String translateEng(int n) {
+    	
+    	
+    	//String[] s = ;
+    	
+    	
+    	/*if(n==0){
+    		return "zero";
+    	}else if(n==1){
+    		return "one";
+    	}else if(n==2){
+    		return "two";
+    	}else if(n==3){
+    		return "three";
+    	}else if(n==4){
+    		return "four";
+    	}else if(n==5){
+    		return "five";
+    	}else if(n==6){
+    		return "six";
+    	}else if(n==7){
+    		return "seven";
+    	}else if(n==8){
+    		return "eight";
+    	}else if(n==9){
+    		return "nine";
+    	}else if(n==10){
+    		return "ten";
+    	}*/
+
+        return "";
+
+    }
+
+}
