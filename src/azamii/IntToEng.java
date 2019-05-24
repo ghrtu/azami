333164095938274f66cab802c3f6981316c85281
aruasani <g17908aa@gm.tsuda.ac.jp>
2019-05-24 13:37:10 +0900

    大幅に改善

--- src/azamii/IntToEng.java
+++ src/azamii/IntToEng.java
@@ -1,8 +1,6 @@
 package azamii;
 
-import java.util.Scanner;
-
-public class IntToEng {
+/*public class IntToEng {
 	
 	// メインメソッド
 
@@ -46,29 +44,90 @@ public class IntToEng {
     	}*/
 
       //  return "";
-
-   // }
-    static  String translateEng(int n){//一の位
-    	String no = " ";
-    	switch(n){
-    	
-    	case 0: no= "zero";
-    	case 1: no= "one";
-    	case 2: no= "two";
-    	case 3: no= "three";
-    	case 4: no= "four";
-    	case 5: no= "five";
-    	case 6: no= "six";
-    	case 7: no= "seven";
-    	case 8: no= "eight";
-    	case 9: no= "nine";
-    	
-    	
-    	}
-    	
+	
+	   // }
+	  /*  static  String translateEng(int n){//一の位
+	    	String no = " ";
+	    	switch(n){
+	    	
+	    	case 0: no= "zero";
+	    	case 1: no= "one";
+	    	case 2: no= "two";
+	    	case 3: no= "three";
+	    	case 4: no= "four";
+	    	case 5: no= "five";
+	    	case 6: no= "six";
+	    	case 7: no= "seven";
+	    	case 8: no= "eight";
+	    	case 9: no= "nine";
+	    	
+	    	
+	    	}
+	    	
     	
     }
    
 
 }
-    
+    */
+
+import java.util.Scanner;
+
+public class IntToEng {
+
+	 // メインメソッド
+	    public static void main(String[] args) {
+	
+	        Scanner sc = new Scanner(System.in);
+	        int input = sc.nextInt();
+	
+	        System.out.println(translateEng(input));
+	
+	    }
+	
+	    // 数値を英訳する変換するメソッド
+	    static String translateEng(int n) {
+	    	//1~19
+	    	String[] eNum1 = {"", "one", "two", "three", "four",
+	    			"five", "six", "seven", "eight", "nine",
+	    			"ten", "eleven", "twelve", "thirteen", "fourteen",
+	    			"fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+	    	//20,30,40,50,60,70,80,90
+	    	String[] eNum2 = {"","","twenty", "thirty", "forty", "fifty", "sixty",
+	    	"seventy", "eighty", "ninety"};
+	    	String num = String.valueOf(n);
+	    	int tenr=n/10;//十の位
+	    	int oner=n%10;//一の位
+	    	int handr=tenr/10;
+	    	String h= "hundred";
+	    	if(n==0){
+	    		num="zero";
+	    		return num;
+	    	}
+	    	else if(n>0&&n<20){
+	    		num =eNum1[n];
+	    		return num;
+	    	}else if(n<100){
+	    		num = eNum2[tenr]+" "+eNum1[oner];
+	    		return num;
+	    	}else if(n==100){
+	    		num = h;
+	    		return num;
+	    		
+	    	}else if(n<120){
+	    		num = eNum1[handr]+" "+h+" "+eNum1[n%100];
+	    		return num;
+	    	}else if(n<1000){
+	    		num= eNum1[handr]+" "+h+" "+eNum2[n%100/10]+" "+eNum1[n%100%10];
+	    		return num;
+	    	}else if(n==1000){
+	    		num = "thousand";
+	    		return num;
+	    	}else {
+	    		num = "範囲外です";
+	    		return num;
+		    	}
+		    	
+		    }
+	}
+
